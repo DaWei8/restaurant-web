196d4275f2f9bd6fb90e34401da68b466b278b8e
David S <sawedavid7@gmail.com>
2022-04-22 17:14:25 +0100

    Updates

---
+++ precache-manifest.8ecffdf3d84f44203e4858c4531fc0cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d413fdf8c20605040418",
+    "url": "/restaurant-web/static/css/main.250b5d9f.chunk.css"
+  },
+  {
+    "revision": "d413fdf8c20605040418",
+    "url": "/restaurant-web/static/js/main.d413fdf8.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/restaurant-web/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "725e771526247c615c5e",
+    "url": "/restaurant-web/static/css/2.01148a48.chunk.css"
+  },
+  {
+    "revision": "725e771526247c615c5e",
+    "url": "/restaurant-web/static/js/2.725e7715.chunk.js"
+  },
+  {
+    "revision": "897f32fabe9fe6cfe534",
+    "url": "/restaurant-web/static/js/runtime~main.897f32fa.js"
+  },
+  {
+    "revision": "8aa7002a66e73e433fc730fbcc99be90",
+    "url": "/restaurant-web/index.html"
+  }
+];
