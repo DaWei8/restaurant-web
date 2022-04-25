7a88a46f8f544bb3e18a3b97853b1c116eb8ad15
David S <sawedavid7@gmail.com>
2022-04-25 11:58:02 +0100

    commit on 'main'

--- src/ProductsPage/ProductsPage.js
+++ src/ProductsPage/ProductsPage.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
 const ProductsPage = () => {
-    return(
+    return (
         <div className='products-page'>
 
         </div>
-        );
+    );
+}
+
+const ProductCard = (props) => {
+    return (
+        <div className='product-card-container'>
+        <div>{props.price}</div>
+        <div><img src=''></img></div>
+        </div>
+    );
 }
 
 export default ProductsPage
